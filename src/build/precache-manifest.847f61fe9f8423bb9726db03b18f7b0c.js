befb90235fbc227030696b239c5e535b6c56bbb4
Nick Ruggero <druggero7@gmail.com>
2019-07-19 19:48:27 -0400

    First commit of backend files

---
+++ src/build/precache-manifest.847f61fe9f8423bb9726db03b18f7b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5efcbd848eb35e714743ffc3f38a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5285e8639bf3db0dabb0",
+    "url": "/static/css/main.ef04f211.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "5285e8639bf3db0dabb0",
+    "url": "/static/js/main.a1c18fc9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
